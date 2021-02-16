c96b45aa032ab592e1e851a6c0b037a7ec6bc912
ArisPapangelis <36777816+ArisPapangelis@users.noreply.github.com>
2021-02-16 03:42:54 +0200

    Version 0.1
    Initial project setup, with first 2 activities and Chaquopy installation.

---
+++ app/src/main/java/com/example/cfiapp/PlottingActivity.java
@@ -0,0 +1,25 @@
+package com.example.cfiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PlottingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plotting);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView7);
+        textView.setText(message);
+
+    }
+}
